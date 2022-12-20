7095baeb42c5321652e60e1258a4a0f3f70b0401
Bikalpa <bikalpapaudel53@gmail.com>
2022-12-20 19:04:20 +0545

    final product's commit

--- src/components/Social.js
+++ src/components/Social.js
@@ -1,9 +1,16 @@
 import React from "react"
+import {AiFillGithub, AiFillTwitterSquare, AiFillLinkedin, AiFillInstagram} from "react-icons/ai"
+import "./Social.css"
 
 export default function Social(){
     return (
         <>
-            <h1>hjfkdak</h1>
+            <section className="social">
+                <a href="https://github.com/Bikalpa-Paudel" className="github"><AiFillGithub className="icons"/></a>
+                <a href="https://twitter.com/Bikalpa003" className="twitter"><AiFillTwitterSquare className="icons"/></a>
+                <a href="https://www.instagram.com/bikalpa_paudel_/" className="Instagram"><AiFillInstagram className="icons"/></a>
+                <a href="https://www.linkedin.com/in/codewith-bikalpa-19b34323a/" className="Linkedin"><AiFillLinkedin className="icons" /></a>
+            </section>
         </>
     )
 }
